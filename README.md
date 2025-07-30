10a5995216d677de01cb70d1311390dcd04d8fdd
uthayakumarrakshi <149175177+uthayakumarrakshi@users.noreply.github.com>
2025-07-30 22:17:57 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+"# telegram_chatbot" 
+"# telegram_chatbot" 
+"# telegram_bot" 
+"# telegram_bot" 
---
+++ config.py
@@ -0,0 +1,11 @@
+
+# Pyrogram setup
+API_ID = 25410154  # Replace this API ID with your actual API ID
+API_HASH = "341628ed7dd229ce550a165372709a12"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7724368964:AAEOAOxhVFkVGuNej8RtLkwZs4waZhGSMwk"  # Replace this BOT_TOKEN
+# Google Api Key
+GOOGLE_API_KEY = "AIzaSyC6ZbnHrrHlXZfjp7n9RSwgB3j4XiRsQjc"  # Replace this Google Api Key
+MODEL_NAME = "gemini-1.5-flash" # Don't Change this model
+
+
+
